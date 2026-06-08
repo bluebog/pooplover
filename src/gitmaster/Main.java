6940b651d582333f05f4808b812b696fd8a5c568
jiho <jihopark0402@gmail.com>
2026-06-08 13:38:54 +0900

    fix merge

--- src/gitmaster/Main.java
+++ src/gitmaster/Main.java
@@ -3,11 +3,9 @@ package gitmaster;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-<<<<<<< HEAD
 		System.out.println("I hate poop");
-=======
 		System.out.println("poop");
->>>>>>> poop
+
 	}
 
 }
